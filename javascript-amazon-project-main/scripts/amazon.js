e27f0756670e13d170a090cf0370d61365230504
More <moreshwargaidhane18@gmail.com>
2026-05-27 12:21:44 +0530

    make cart quantity interactive button

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -81,6 +81,14 @@ button.addEventListener('click', () => {
     productId: productId,
     quantity: 1
   });}
-  console.log(cart);
+
+  let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+  
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+ /* console.log(cart);
+    console.log(cartQuantity);*/ //for learning purposes only, to see the cart and cart quantity in the console
 });
 });
