1e4ba76b21a25679111a5ae85288aef74dd28669
More <moreshwargaidhane18@gmail.com>
2026-06-02 22:47:38 +0530

    remove defalut test and money test

---
+++ javascript-amazon-project-main/test-jasmine/moneytest.js
@@ -0,0 +1,15 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+describe ('test suit: formatCurrency', () => {
+  it('convert cents into dollars', () => {
+    expect(formatCurrency(2095)).toEqual('20.95');        //basic test case
+  });
+
+  it('works with zero cents', () => {
+    expect(formatCurrency(0)).toEqual('0.00');                   //edge case
+  } );
+
+  it('rounds to the nearest cent', () => {
+    expect(formatCurrency(2000.5)).toEqual('20.01');  //In testing, an edge case is a test case that checks the behavior of a program at the boundary or extreme values of input
+  } );
+} )
