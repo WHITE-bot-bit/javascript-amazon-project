97d102a7d2ad40f9e70ed9b24aadbfcbb74b74b0
More <moreshwargaidhane18@gmail.com>
2026-07-28 16:36:20 +0530

    OOP

---
+++ javascript-amazon-project-main/data/cart-oop.js
@@ -0,0 +1,95 @@
+
+
+const cart = {
+   cartItems: undefined,
+
+
+     loadfromstorage: function(){
+this.cartItems = JSON.parse(localStorage.getItem('cart-oop')) || //if there is no cart in local storage then we will use empty array as default value
+
+ [
+  {
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryoptionid:'1'
+  },
+   {
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryoptionid:'2'
+  }
+];
+} ,
+
+
+ savetostorage(){         //short hand for function definition
+  localStorage.setItem('cart-oop',JSON.stringify(this.cartItems));
+},
+
+
+  addtocart(productId) {
+let matchingItem;
+    this.cartItems.forEach((CartItem) => {
+      if (CartItem.productId === productId) {//👉 to uniquely identify each product name are not reliable
+        matchingItem = CartItem;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity++;
+    } else {
+  this.cartItems.push({
+    productId: productId,
+    quantity: 1,
+    deliveryoptionid:'1'
+  });}
+this.savetostorage();
+},
+
+
+  removefromcart(productId) {
+      const newcart =[];
+      this.cartItems.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+          newcart.push(cartItem);
+        } });
+
+        this.cartItems = newcart;
+
+        this.savetostorage();
+  },
+  /*Create empty cart
+//Check every item
+//Skip the item to remove
+//Put all remaining items into new cart
+//Replace old cart with new cart*/
+
+
+
+
+ udatedeliveryoption(productId,deliveryoptionid){
+  let matchingItem;
+
+    this.cartItems.forEach((CartItem) => {
+      if (CartItem.productId === productId) {
+        matchingItem = CartItem;
+      }
+    });
+
+    matchingItem.deliveryoptionid = deliveryoptionid;
+
+    this.savetostorage();
+
+}
+
+};
+
+
+cart.loadfromstorage();
+
+
+
+
+
+console.log(cart);
+console.log(buisnesscart);
--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -2,5 +2,7 @@ import {renderordersummary} from './checkout/ordersummary.js';
 
 import {renderpaymentsummary} from './checkout/paymentsummary.js';
 
+import '../data/cart-oop.js';
+
 renderordersummary();
 renderpaymentsummary();
